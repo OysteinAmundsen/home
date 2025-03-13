93cd19911515cd2e78b5bc795a73d712f4c1bda4
Oystein Amundsen <oystein.amundsen@gmail.com>
2025-03-13 21:06:58 +0100

    Trying out echarts

--- libs/widget/fund/src/lib/fund.component.ts
+++ libs/widget/fund/src/lib/fund.component.ts
@@ -1,43 +1,51 @@
-import { CommonModule } from '@angular/common';
-import { Component, computed, effect, ElementRef, inject, OnInit, signal, viewChild } from '@angular/core';
-import { ResizeDirective } from '@home/shared/browser/resize/resize.directive';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, effect, inject, linkedSignal, OnInit, signal } from '@angular/core';
 import { AbstractWidgetComponent } from '@home/shared/widget/abstract-widget.component';
 import { WidgetComponent } from '@home/shared/widget/widget.component';
+import { LineChart } from 'echarts/charts';
+import { GridComponent } from 'echarts/components';
+import * as echarts from 'echarts/core';
+import { CanvasRenderer } from 'echarts/renderers';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 import { firstValueFrom } from 'rxjs';
 import { FundService } from './fund.service';
 
+if (typeof window !== 'undefined') {
+  echarts.use([LineChart, GridComponent, CanvasRenderer]);
+}
+
 @Component({
   selector: 'lib-fund',
-  imports: [CommonModule, WidgetComponent, ResizeDirective],
+  imports: [CommonModule, WidgetComponent, NgxEchartsDirective],
   templateUrl: './fund.component.html',
   styleUrl: './fund.component.scss',
-  providers: [{ provide: FundService }],
+  providers: [{ provide: FundService }, provideEchartsCore({ echarts })],
 })
 export default class FundComponent extends AbstractWidgetComponent implements OnInit {
   private readonly fundService = inject(FundService);
-  graphCanvas = viewChild('graph', { read: ElementRef });
-  ctx = computed(() => this.graphCanvas()?.nativeElement.getContext('2d'));
 
   override id = signal('fund');
   instruments = signal<any[]>([]);
+  canRender = linkedSignal(() => isPlatformBrowser(this.platformId));
+  api = signal<echarts.ECharts | undefined>(undefined);
 
-  // Size
-  rect = signal<DOMRect>({ width: 0, height: 0 } as DOMRect);
-  width = computed(() => this.rect().width);
-  height = computed(() => this.rect().height);
+  chartOption: echarts.EChartsCoreOption = {
+    xAxis: {
+      type: 'time',
+    },
+    yAxis: {
+      type: 'value',
+      name: 'NOK',
+    },
+  };
 
-  // Graph colors
-  private colors = ['#FF5733', '#33FF57', '#3357FF', '#FF33A1', '#A133FF', '#33FFF5', '#FF8C33', '#8C33FF'];
+  mergeOptions = signal<echarts.EChartsCoreOption | undefined>(undefined);
 
-  onInstrumentChanged = effect(() => {
-    // Triggers
-    const data = this.instruments();
-    const canvas = this.graphCanvas()?.nativeElement;
-
-    if (data.length && this.width() > 0 && this.height() > 0) {
-      data.forEach((item: any, index: number) => {
-        this.createGraph(canvas, item.timeSeries.pricePoints, index);
-      });
+  onDataChanged = effect(() => {
+    const api = this.api();
+    const options = this.mergeOptions();
+    if (api && options) {
+      api.setOption(options);
     }
   });
 
@@ -46,51 +54,38 @@ export default class FundComponent extends AbstractWidgetComponent implements On
       const data = await firstValueFrom(this.fundService.getFundData());
 
       // For each instrument, fetch price time series
+      const timeSeries = [] as { timeStamp: number; value: number }[][];
       await Promise.allSettled(
         data.map(async (item: any) => {
           const res = await firstValueFrom(this.fundService.getTimeSeries(item.nnx_info.market_data_order_book_id));
           item.timeSeries = res;
+          timeSeries.push(res.pricePoints);
         }),
       );
-      this.instruments.set(data);
+
+      const options = {
+        xAxis: {
+          type: 'time',
+          data: Array.from(
+            timeSeries
+              .flatMap((point) => point)
+              .map((p) => p.timeStamp)
+              .reduce((acc, cur) => acc.add(cur), new Set()),
+          ),
+        },
+        series: data.map((item: any) => ({
+          name: item.instrument_info.long_name,
+          type: 'line',
+          data: item.timeSeries.pricePoints.map((p: any) => [p.timeStamp, p.value]),
+        })),
+      };
+      this.mergeOptions.set(options);
     } catch (error) {
       console.error('Error fetching fund data', error);
     }
   }
 
-  onResize(size: DOMRect) {
-    this.rect.set(size);
-  }
-
-  private createGraph(
-    canvas: HTMLCanvasElement,
-    pricePoints: { timestamp: number; value: number }[],
-    index: number,
-  ): void {
-    const ctx = this.ctx();
-    if (ctx) {
-      const color = this.colors[index % this.colors.length];
-
-      // ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.beginPath();
-      ctx.strokeStyle = color;
-
-      const xScale = canvas.width / (pricePoints.length - 1);
-      const yMax = Math.max(...pricePoints.map((p) => p.value));
-      const yMin = Math.min(...pricePoints.map((p) => p.value));
-      const yScale = canvas.height / (yMax - yMin);
-
-      pricePoints.forEach((point, i) => {
-        const x = xScale * i;
-        const y = canvas.height - (point.value - yMin) * yScale;
-        if (i === 0) {
-          ctx.moveTo(x, y);
-        } else {
-          ctx.lineTo(x, y);
-        }
-      });
-
-      ctx.stroke();
-    }
+  onChartInit($event: echarts.ECharts) {
+    this.api.set($event);
   }
 }
