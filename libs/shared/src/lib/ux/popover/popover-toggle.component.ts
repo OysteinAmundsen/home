b0d3cc0a3e041e7cec23554181c94ad30793985d
Oystein Amundsen <oystein.amundsen@gmail.com>
2025-03-18 15:40:38 +0100

    fix: a popover toggle requires a popover component

--- libs/shared/src/lib/ux/popover/popover-toggle.component.ts
+++ libs/shared/src/lib/ux/popover/popover-toggle.component.ts
@@ -4,19 +4,17 @@ import { PopoverComponent } from './popover.component';
 @Component({
   selector: 'lib-popover-toggle',
   template: `
-    @if (popoverPanel()) {
-      <button
-        type="button"
-        class="popover-toggle"
-        [attr.popovertarget]="popoverPanel()?.uniqueId()"
-        [style]="'anchor-name: --' + popoverPanel()?.uniqueId()"
-        popovertargetaction="toggle"
-      >
-        <span class="material-symbols-outlined">more_vert</span>
-      </button>
-    }
+    <button
+      type="button"
+      class="popover-toggle"
+      [attr.popovertarget]="popoverPanel().uniqueId()"
+      [style]="'anchor-name: --' + popoverPanel().uniqueId()"
+      popovertargetaction="toggle"
+    >
+      <span class="material-symbols-outlined">more_vert</span>
+    </button>
   `,
 })
 export class PopoverToggleComponent {
-  popoverPanel = input<PopoverComponent>();
+  popoverPanel = input.required<PopoverComponent>();
 }
