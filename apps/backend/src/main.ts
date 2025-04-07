b2169c23986029ec951a5d2a7fcc041c972c3dc7
Oystein Amundsen <oystein.amundsen@gmail.com>
2025-04-07 21:52:33 +0200

    fix: typeroots and added a devcontainer

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,7 +2,7 @@ import { Logger } from '@nestjs/common';
 import { createServer } from './index';
 
 async function bootstrap() {
-  const { app } = await createServer();
+  const app = await createServer();
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     Logger.log(`Backend server listening on http://localhost:${port}`);
